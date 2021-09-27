0804cff6c3bc03001e637fde71b9a391342c2467
Hannes Achleitner <hannes.software@gmx.at>
2021-09-27 07:38:15 +0200

    fixup! Introduce a viro version variable

--- android/mainSample/src/main/java/com/example/virosample/SystemInfoFragment.kt
+++ android/mainSample/src/main/java/com/example/virosample/SystemInfoFragment.kt
@@ -14,7 +14,11 @@ class SystemInfoFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         // Load the preferences from an XML resource
         addPreferencesFromResource(R.xml.prefs)
+        // TODO after next release you can use info from library
+        //findPreference<Preference>(PREFERENCE_ + "APPVERSION")?.summary = com.viro.renderer.BuildConfig.VIRO_VERSION
         findPreference<Preference>(PREFERENCE_ + "APPVERSION")?.summary = BuildConfig.VERSION_NAME
+        findPreference<Preference>(PREFERENCE_ + "APPVERSION")?.title =
+            "${findPreference<Preference>(PREFERENCE_ + "APPVERSION")?.title} [${BuildConfig.FLAVOR}]"
         findPreference<Preference>(PREFERENCE_ + "BOARD")?.summary = Build.BOARD
         findPreference<Preference>(PREFERENCE_ + "BRAND")?.summary = Build.BRAND
         findPreference<Preference>(PREFERENCE_ + "CPU_ABI")?.summary = Build.SUPPORTED_ABIS[0]
