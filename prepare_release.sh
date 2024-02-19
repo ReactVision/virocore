b8c28f070faacab63df5f21a4f6b81ef7cf9eaf3
Robert Colley <robert.colley04@gmail.com>
2024-02-19 15:22:36 -0500

    fix: virocore build
    - Adds build script

---
+++ prepare_release.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo '========================================================================='
+echo 'Cleaning :viroreact'
+echo '========================================================================='
+cd android &&
+./gradlew clean
+
+echo '========================================================================='
+echo 'Building :viroreact'
+echo '========================================================================='
+ ./gradlew :viroreact:assembleRelease
+
+echo '========================================================================='
+echo 'Checking for build artifacts'
+echo '========================================================================='
+if [ ! -f viroreact/build/outputs/aar/viroreact-release.aar ]; then
+    echo -e "Unable to find viroreact release output!"
+    exit 1
+fi
